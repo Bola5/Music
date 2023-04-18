da1e30fc14e96b144221c8bf117b74caac40ef48
Bola Fayez <bola.fayez@yahoo.com>
2023-04-18 23:54:04 +0200

    Feat: Add MusicViewModel

--- Music/Business Logic/ViewModel/MusicViewModel.swift
+++ Music/Business Logic/ViewModel/MusicViewModel.swift
@@ -5,25 +5,78 @@
 //  Created by Bola Fayez on 18/04/2023.
 //
 
-import UIKit
+import Foundation
 
-class MusicViewModel: UIViewController {
+protocol MusicViewModelProtocol {
+    
+    //MARK: - Protocol - Data Source
+    var countOfMusics: Int { get }
+    func musicAt(index: Int) -> MusicLayoutViewModel.ResultsLayoutViewModel?
+    func shouldFetchNextPage(index: Int) -> Bool
+    
+    // MARK: - Protocol - fetch
+    func fetchMusics(term: String, completion: @escaping MusicViewModel.GetMusicCompletionBlock)
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class MusicViewModel: MusicViewModelProtocol {
 
-        // Do any additional setup after loading the view.
+    // MARK: - Callback type alias
+    typealias GetMusicCompletionBlock = (Result<Bool, Error>) -> Void
+
+    // MARK: - Properties
+    private var limit: Int = 20
+    private var nextPage: Int = 1
+    // Data Source
+    private let musicRemoteDataSource: MusicRemoteDataSourceProtocol
+    private var layoutViewModel: MusicLayoutViewModel?
+    var countOfMusics: Int {
+        return layoutViewModel?.results.count ?? 0
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Init
+    init(musicRemoteDataSource: MusicRemoteDataSourceProtocol = MusicRemoteDataSource()) {
+        
+        self.musicRemoteDataSource = musicRemoteDataSource
     }
-    */
+    
+}
 
+// MARK: - Protocol - Data Source method
+extension MusicViewModel {
+    
+    func musicAt(index: Int) -> MusicLayoutViewModel.ResultsLayoutViewModel? {
+        return self.layoutViewModel?.results[index]
+    }
+    
+    func shouldFetchNextPage(index: Int) -> Bool {
+        return index == countOfMusics - 4
+    }
+
+}
+
+// MARK: - Protocol - fetch
+extension MusicViewModel {
+    
+    func fetchMusics(term: String, completion: @escaping GetMusicCompletionBlock) {
+        musicRemoteDataSource.fetchMusics(term: term, limit: limit, completion: { [weak self] result in
+            switch result {
+            case .success(let musicModel):
+                self?.nextPage += 1
+                self?.updateLimit()
+                self?.layoutViewModel = MusicLayoutViewModel(musics: musicModel)
+                completion(.success(true))
+            case .failure(let error):
+                if (self?.nextPage ?? 0) >= 2 {
+                    self?.nextPage -= 1
+                    self?.limit -= (self?.limit ?? 0)
+                }
+                completion(.failure(error))
+            }
+        })
+    }
+
+    private func updateLimit() {
+        limit += limit
+    }
+    
 }
