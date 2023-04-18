dc6eed6d88c88b02a80a95f10bff32da1ede5cfa
Bola Fayez <bola.fayez@yahoo.com>
2023-04-18 22:28:16 +0200

    Feat: Add Constants file

---
+++ Music/Core/AUX/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Music
+//
+//  Created by Bola Fayez on 18/04/2023.
+//
+
+import Foundation
+
+enum Strings {
+    
+    enum Titles {
+        static let MUSIC_TITLE = "Music"
+        static let OK_TITLE = "OK"
+    }
+    
+    enum Image {
+        static let PLACEHOLDER_IMAGE = "PlaceholderImage"
+        static let PREFIX_IMAGE = "image"
+    }
+    
+    enum URL {
+        static let BASE_URL = "https://itunes.apple.com/"
+        static let ENDPOINT = "search"
+        static let MEDIA = "music"
+        static let COUNTRY_CODE = "dk"
+    }
+}
