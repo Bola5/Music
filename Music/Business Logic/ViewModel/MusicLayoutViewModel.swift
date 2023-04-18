d7e258e2baa8cf07bb636227ee3ee81b4ce89e73
Bola Fayez <bola.fayez@yahoo.com>
2023-04-18 22:45:52 +0200

    Feat: Add MusicLayoutViewModel

---
+++ Music/Business Logic/ViewModel/MusicLayoutViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  MusicLayoutViewModel.swift
+//  Music
+//
+//  Created by Bola Fayez on 18/04/2023.
+//
+
+import Foundation
+
+struct MusicLayoutViewModel {
+
+    let results: [ResultsLayoutViewModel]
+
+    init(musics: MusicModel) {
+        self.results = musics.results.compactMap({ ResultsLayoutViewModel(music: $0) })
+    }
+
+    struct ResultsLayoutViewModel {
+        
+        let trackName: String?
+        let artistName: String?
+        let releaseDate: String?
+        let artworkUrl: String?
+        let description: String?
+        
+        init(music: MusicModel.Results) {
+            self.trackName = music.trackName
+            self.artistName = music.artistName
+            self.releaseDate = music.releaseDate
+            self.artworkUrl = music.artworkUrl60
+            self.description = music.collectionName
+        }
+        
+    }
+    
+}
---
+++ Music/Business Logic/ViewModel/MusicViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  MusicViewModel.swift
+//  Music
+//
+//  Created by Bola Fayez on 18/04/2023.
+//
+
+import UIKit
+
+class MusicViewModel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
